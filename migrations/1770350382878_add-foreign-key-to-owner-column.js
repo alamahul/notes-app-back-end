d26494cd6fd489753f67ee37c0571d3c3256231e
alamahul <alamahulgrt0000@gmail.com>
2026-02-06 11:05:51 +0700

    add-foreign-key

---
+++ migrations/1770350382878_add-foreign-key-to-owner-column.js
@@ -0,0 +1,24 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+    pgm.sql("INSERT INTO users(id, username, password, fullname, created_at, updated_at) VALUES ('old_notes', 'old_notes', 'old_notes', 'old notes', NOW(), NOW())");
+    // mengubah nilai owner pada note yang owner-nya bernilai NULL
+    pgm.sql("UPDATE notes SET owner = 'old_notes' WHERE owner IS NULL");
+    // memberikan constraint foreign key pada owner terhadap kolom id dari tabel users
+    pgm.addConstraint('notes', 'fk_notes.owner_users.id', 'FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE');
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => { };
